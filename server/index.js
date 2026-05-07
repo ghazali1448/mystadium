9ef75dd2a70c63c8150f81b976a8ce884446cc15
ghazali1448 <hamilghazali69@gmail.com>
2026-05-07 22:35:44 +0100

    Fix Express v5 wildcard route

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
 
 // Catch-all to serve frontend index.html for any non-API route
-app.get('*', (req, res) => {
+app.get('{*path}', (req, res) => {
   if (!req.url.startsWith('/api')) {
     res.sendFile(path.join(distPath, 'index.html'));
   } else {
